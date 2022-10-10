abdcbd4b3d385fde0fadcc99c93a1610d806f223
Lucas Frazão <lucas_devfrazao@outlook.com>
2022-10-10 09:26:09 -0300

    Title screen build

---
+++ OOP_Project/Assets/Scripts/MenuUI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MenuUI : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#endif
+        Application.Quit();
+    }
+}
